9cd105b0939a36d1d21545537f1890a3c4410989
vinay kumar <87534850+687vinay@users.noreply.github.com>
2021-07-18 10:34:19 +0530

    Update logistic_titanicdataset .py

--- logistic_titanicdataset .py
+++ logistic_titanicdataset .py
@@ -3,30 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns 
 
-'''
+
 
 features(['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp',
        'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked', 'WikiId', 'Name_wiki',
        'Age_wiki', 'Hometown', 'Boarded', 'Destination', 'Lifeboat', 'Body',
        'Class'],
       dtype='object')
-'''
+
+
 file_path='C:/Users/DELL/Desktop/dataets/train.csv'
 train_data=pd.read_csv(file_path)  
 
-#print(train_data)
+print(train_data)
 
 
 
-#print(train_data.columns)
+print(train_data.columns)
 
 ## dreoping the usless features from train data_sets;
 train_data.drop(['Age_wiki', 'Hometown','Destination','Name_wiki','WikiId','Cabin','Age_wiki','Ticket','Name','Fare','PassengerId'],inplace=True,axis=1)
 
 train_data.drop(['Lifeboat','Boarded','SibSp'],inplace=True,axis=True)
 train_data.drop(['Body'],inplace=True,axis=1)
-#print(train_data.shape)
-#print(train_data.head(10))
+
+print(train_data.shape)
+
 
 
 
@@ -38,7 +40,6 @@ train_data.dropna(inplace=True)
 
 print(train_data.isnull().sum())
 
-#print(X_train.shape," ",y_train.shape)
 
 ## changing the datatype 
 train_data["Age"]=train_data["Age"].astype("int64")
@@ -46,8 +47,8 @@ train_data["Survived"]=train_data["Survived"].astype("int64")
 train_data["Class"]=train_data["Class"].astype("int64")
 
 
-## convert clasiification into 0 OR 1;
-#print(train_data.head())
+
+print(train_data.head())
 
 embark = pd.get_dummies(train_data["Embarked"],drop_first=True)
 
@@ -65,8 +66,9 @@ train_data=pd.concat([train_data,sex,embark,pclass],axis=1)
 
 X_train=train_data.iloc[:,1:]
 y_train=train_data.iloc[:,0]
-#print(X_train)
-'''
+
+## logistic regression from scratch;
+
 ## define the sigmoid function;
 def sigmoid(score):
        return 1/(1+np.exp(-score))
@@ -114,17 +116,15 @@ print(weights)
 
 
 ## accuracy:
-
-##data_with_intercept=np.hstack((np.ones((X_train.shape[0],1)),y_train))
+data_with_intercept=np.hstack((np.ones((X_train.shape[0],1)),y_train))
 ##simulated_labels).sum().astype(float) / len(preds)))final_score=np.dot(data_with_intercept,weights)
-'''
-'''
+
+
 preds=np.rounp.concatenatend(sigmoid(final_score))
 
 print("Accuracy from scratch:{0}".formate((preds == simulated_labels).sum().astype(float) / len(preds)))
 
-'''
-'''
+##acuracy by using sklearn;
 from sklearn.linear_model import LogisticRegression
 
 clf = LogisticRegression(fit_intercept=True, C = 1e15)
